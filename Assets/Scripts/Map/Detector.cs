4c0857dd0accca78edd11ff9f9969da8ee7979f5
Xwilarg <xwilarg@protonmail.com>
2024-08-01 15:17:03 +0200

    Add detector

---
+++ Assets/Scripts/Map/Detector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PotionMorph.Map
+{
+    public class Detector : MonoBehaviour
+    {
+        private List<GameObject> _contained = new();
+        public IReadOnlyList<GameObject> Contained => _contained.AsReadOnly();
+
+        public UnityEvent<GameObject> OnAdded { get; } = new();
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            _contained.Add(collision.gameObject);
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            _contained.Remove(collision.gameObject);
+        }
+    }
+}
