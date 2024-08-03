11453448d27df70ca534290d80bb77fdd8d7199b
Xwilarg <xwilarg@protonmail.com>
2024-08-03 13:03:19 +0200

    Add detector checks

--- Assets/Scripts/Map/Ingredient.cs
+++ Assets/Scripts/Map/Ingredient.cs
@@ -5,7 +5,6 @@ namespace PotionMorph.Map
     public class Ingredient : MonoBehaviour, IProp
     {
         public bool CanGrab => true;
-        public bool CanReceiveIngredient => false;
 
         public Rigidbody2D Rigidbody { private set; get; }
         public IMachine AssociatedMachine { set; get; }
--- Assets/Scripts/Map/Machine.cs
+++ Assets/Scripts/Map/Machine.cs
@@ -1,5 +1,6 @@
 using PotionMorph.Manager;
 using PotionMorph.Map;
+using System.Linq;
 using UnityEngine;
 
 public class Machine : MonoBehaviour, IMachine
@@ -8,6 +9,25 @@ public class Machine : MonoBehaviour, IMachine
     private Transform _slotPos;
 
     private IProp _registeredProp;
+    private Detector _detector;
+
+    private void Awake()
+    {
+        _detector = GetComponentInChildren<Detector>();
+        _detector.OnAdded.AddListener((go) => // When an ingredient is added, we check if it was be mixed
+        {
+            if (_registeredProp != null && go.TryGetComponent<Ingredient>(out var ingredient))
+            {
+                Consume(ingredient);
+            }
+        });
+    }
+
+    private void Consume(Ingredient ingredient)
+    {
+        GameManager.Instance.Drop(ingredient); // Make sure we aren't holding it
+        Destroy(ingredient.gameObject);
+    }
 
     public void Unregister(IProp prop)
     {
@@ -17,13 +37,20 @@ public class Machine : MonoBehaviour, IMachine
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.TryGetComponent<IProp>(out var prop) && prop.CanReceiveIngredient && _registeredProp == null)
+        if (collision.TryGetComponent<Container>(out var container) && container.CanReceiveIngredient && _registeredProp == null)
         {
-            GameManager.Instance.Drop(prop);
-            prop.Rigidbody.bodyType = RigidbodyType2D.Kinematic;
-            prop.Rigidbody.linearVelocity = Vector2.zero;
-            prop.Rigidbody.transform.position = _slotPos.position;
-            _registeredProp = prop;
+            GameManager.Instance.Drop(container);
+            container.Rigidbody.bodyType = RigidbodyType2D.Kinematic;
+            container.Rigidbody.linearVelocity = Vector2.zero;
+            container.Rigidbody.transform.position = _slotPos.position;
+            _registeredProp = container;
+
+            // We put a glass under, is there any ingredient waiting to be mixed?
+            var ingredient = _detector.Contained.Select(x => x.GetComponent<Ingredient>()).FirstOrDefault(x => x != null);
+            if (ingredient != null)
+            {
+                Consume(ingredient);
+            }
         }
     }
 }
