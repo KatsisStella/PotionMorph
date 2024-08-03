58eaf36f82ae90ba7d70c6dad4897647a414b6f7
Xwilarg <xwilarg@protonmail.com>
2024-08-03 13:05:08 +0200

    Fix fruits not being pressed

--- Assets/Scripts/Map/Detector.cs
+++ Assets/Scripts/Map/Detector.cs
@@ -6,7 +6,7 @@ namespace PotionMorph.Map
 {
     public class Detector : MonoBehaviour
     {
-        private List<GameObject> _contained = new();
+        private readonly List<GameObject> _contained = new();
         public IReadOnlyList<GameObject> Contained => _contained.AsReadOnly();
 
         public UnityEvent<GameObject> OnAdded { get; } = new();
@@ -14,6 +14,7 @@ namespace PotionMorph.Map
         private void OnTriggerEnter2D(Collider2D collision)
         {
             _contained.Add(collision.gameObject);
+            OnAdded?.Invoke(collision.gameObject);
         }
 
         private void OnTriggerExit2D(Collider2D collision)
