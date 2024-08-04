04765e5dd0caa1a1ff2624f53fe1c0eae30634b3
Xwilarg <xwilarg@protonmail.com>
2024-08-04 11:46:04 +0200

    Add recipe manager

---
+++ Assets/Scripts/Manager/RecipeManager.cs
@@ -0,0 +1,35 @@
+﻿using PotionMorph.Map;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace PotionMorph.Manager
+{
+    public class RecipeManager : MonoBehaviour
+    {
+        public static RecipeManager Instance { private set; get; }
+
+        [SerializeField]
+        private TMP_Text _recipeText;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public void ShowRecipeText(Ingredient[] ingredients)
+        {
+            _recipeText.gameObject.SetActive(true);
+
+            _recipeText.text = "Unidentified mix";
+
+            StartCoroutine(RemoveRecipeText());
+        }
+
+        private IEnumerator RemoveRecipeText()
+        {
+            yield return new WaitForSeconds(3f);
+            _recipeText.gameObject.SetActive(false);
+        }
+    }
+}
