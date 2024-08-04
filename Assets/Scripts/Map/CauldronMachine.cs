250c4c09b8f8b759d8354b9f97aa0b0618059404
Xwilarg <xwilarg@protonmail.com>
2024-08-04 11:34:16 +0200

    Improve spatula management

--- Assets/Scripts/Map/CauldronMachine.cs
+++ Assets/Scripts/Map/CauldronMachine.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using PotionMorph.Manager;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -11,17 +12,43 @@ namespace PotionMorph.Map
 
         private readonly List<Container> _ingredients = new();
 
+        private Vector3 _baseSpatulaPos;
+
         protected override void Awake()
         {
             base.Awake();
+
+            _baseSpatulaPos = _spatula.transform.position;
+
+            // Put back spatula on the cauldron
+            _detector.OnAdded.AddListener((go) =>
+            {
+                if (go.CompareTag("Spatula"))
+                {
+                    _spatula.Rigidbody.linearVelocity = Vector2.zero;
+                    _spatula.Rigidbody.bodyType = RigidbodyType2D.Kinematic;
+                    _spatula.transform.position = _baseSpatulaPos;
+                    _spatula.AssociatedMachine = this;
+                    _spatula.CanGrab = false;
+                    _container = _spatula;
+                }
+            });
+        }
+
+        private void Start()
+        {
             _container = _spatula;
             _spatula.Rigidbody.bodyType = RigidbodyType2D.Kinematic;
             _spatula.CanGrab = false;
+            _spatula.AssociatedMachine = this;
         }
 
         protected override bool CanTreat(GameObject go, out Container output)
         {
-            return base.CanTreat(go, out output) && output.Ingredients.Any();
+            return base.CanTreat(go, out output) &&
+                _spatula.AssociatedMachine != null && // We only add things on the cauldron if the spatula is there
+                !go.CompareTag("Spatula") && // The spatula can't count as an ingredient
+                output.Ingredients.Any(); // We are not interested in empty containers
         }
 
         protected override void TreatConsumption(Container ingredient)
@@ -34,6 +61,20 @@ namespace PotionMorph.Map
             {
                 _spatula.Fill(_ingredients.SelectMany(x => x.Ingredients).ToArray());
                 _spatula.CanGrab = true;
+            }
+        }
+
+        protected override void DenyConsumption(GameObject ingredient)
+        {
+            base.DenyConsumption(ingredient);
+
+            if (ingredient.TryGetComponent<Rigidbody2D>(out var rb))
+            {
+                if (ingredient.TryGetComponent<IProp>(out var prop))
+                {
+                    GameManager.Instance.Drop(prop);
+                }
+                rb.AddForce(new Vector2(Random.Range(-.25f, .25f), 1f).normalized * 20f, ForceMode2D.Impulse);
             }
         }
     }
